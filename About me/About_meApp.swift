c81f5d09c6642e392e8c1e4fbc1ba6ef30ea5ad1
LamiaAlsiddiqi <lwalsiddiqi22@twq.idserve.net>
2022-10-27 09:54:56 +0300

    Initial Commit

---
+++ About me/About_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_meApp.swift
+//  About me
+//
+//  Created by Lamia AlSiddiqi on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct About_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About me/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  About me
+//
+//  Created by Lamia AlSiddiqi on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            //uuu
+            VStack{
+                Image("horse")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(101)
+                Text("Lamia")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                
+                Text("A Speech and Communication Therapist. Photographer, inventor, passionate in art and creativity, an admirer of horseback riding.")
+                    .font(.body)
+                    .fontWeight(.regular)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                
+                    .padding()
+                
+            }}
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
